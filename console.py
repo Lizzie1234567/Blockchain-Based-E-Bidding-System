8162013a8282d6ea4ada6a0f956c1d606ff6659e
stareck <847994565@qq.com>
2022-11-24 00:01:02 +0800

    1

--- console.py
+++ console.py
@@ -1,5 +1,6 @@
 # coding:utf-8
 from account import *
+from miner import mine
 from rpc import get_clients, broadCast, start_server
 from data import *
 from database import *
@@ -12,18 +13,18 @@ from node import *
 from lib.common import cprint
 import inspect
 
-MODULES = ['account','tx','blockchain','miner','node']
+MODULES = ['account', 'tx', 'blockchain', 'miner', 'node']
+
 
 def upper_first(string):
-    return string[0].upper()+string[1:]
-
+    return string[0].upper() + string[1:]
 
 
 class node():
 
-    def add(self, args,a):
-        add_node(args[0],a)
-        rpc.broadCast().add_node(args[0],a)
+    def add(self, args, a):
+        add_node(args[0], a)
+        rpc.broadCast().add_node(args[0], a)
         cprint('Allnode', get_nodes())
 
     def run(self, args):
@@ -33,59 +34,66 @@ class node():
         for t in NodeDB().find_all():
             cprint('Node', t)
 
+
 class miner():
     def start(self, args):
-        if get_account() == None:
-            cprint('ERROR','Please create account before start miner.')
+        if get_S_account() == None:
+            cprint('ERROR', 'Please create account before start miner.')
             exit()
         start_node(PoA())
-        while True :
-            cprint('Miner new block',mine().to_dict())
+        while True:
+            cprint('Miner new block', mine().to_dict())
+
 
 class Account():
     def create(self, args):
         ac = new_account()
-        cprint('Private Key',ac[0])
-        cprint('Public Key',ac[1])
-        cprint('Address',ac[2])
+        cprint('Private Key', ac[0])
+        cprint('Public Key', ac[1])
+        cprint('Address', ac[2])
 
     def get(self, args):
-        cprint('All Account',AccountDB().read())
+        cprint('All Account', AccountDB().read())
 
     def current(self, args):
         cprint('Current Account', get_account())
+
 
 class Blockchain():
 
     def list(self, args):
         for t in BlockChainDB().find_all():
-            cprint('Blockchain',str(t))
+            cprint('Blockchain', str(t))
+
 
 class Tx():
 
     def list(self, args):
         for t in DataDB().find_all():
-            cprint('Data',t)
+            cprint('Data', t)
 
     def transfer(self, args):
-        tx = AData.transfer(args[0], args[1], args[2],args)
+        tx = AData.transfer(args[0], args[1], args[2], args)
         print(Data.unblock_spread(tx))
-        cprint('Data tranfer',tx)
+        cprint('Data tranfer', tx)
+
 
 def usage(class_name):
     module = globals()[upper_first(class_name)]
     print('  ' + class_name + '\r')
     print('    [action]\r')
-    for k,v in module.__dict__.items():
+    for k, v in module.__dict__.items():
         if callable(v):
             print('      %s' % (k,))
     print('\r')
+
 
 def help():
     print("Usage: python console.py [module] [action]\r")
     print('[module]\n')
     for m in MODULES:
         usage(m)
+
 
 if __name__ == '__main__':
     if len(sys.argv) == 1:
@@ -96,7 +104,7 @@ if __name__ == '__main__':
         help()
         exit()
     if module not in MODULES:
-        cprint('Error', 'First arg shoud in %s' % (str(MODULES,)))
+        cprint('Error', 'First arg shoud in %s' % (str(MODULES, )))
         exit()
     mob = globals()[upper_first(module)]()
     method = sys.argv[2]
